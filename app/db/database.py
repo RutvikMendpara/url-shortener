66d1f5848b9a6e6962d33576894e35e4d4a590a0
Rutvik Mendpara <rutvikm200@gmail.com>
2026-03-10 12:23:41 +0530

    Add Dockerfile and update docker-compose for Redis and PostgreSQL integration

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/url_shortener"
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5433/url_shortener"
+)
 
 engine = create_engine(DATABASE_URL)
 
--- app/queue.py
+++ app/queue.py
@@ -1,6 +1,13 @@
+import os
 from redis import Redis
 from rq import Queue
 
-redis_conn = Redis(host="localhost", port=6379)
+redis_host = os.getenv("REDIS_HOST", "localhost")
+
+redis_conn = Redis(
+    host=redis_host,
+    port=6379,
+    decode_responses=True
+)
 
 queue = Queue("analytics", connection=redis_conn)
