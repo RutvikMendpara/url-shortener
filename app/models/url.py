ee11612a81afaa56dd2d241df260848c1c5876bc
Rutvik Mendpara <rutvikm200@gmail.com>
2026-03-10 10:15:09 +0530

    Add database configuration, URL model, and Docker setup for URL shortener

---
+++ app/models/url.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Text, Timestamp
+from sqlalchemy.sql import func
+from app.db.database import Base
+from sqlalchemy import DateTime
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    short_code = Column(String(10), unique=True, index=True, nullable=False)
+    original_url = Column(Text, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    expires_at = Column(DateTime(timezone=True), nullable=True)
+
+    click_count = Column(Integer, default=0)
+
+
